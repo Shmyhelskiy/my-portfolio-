806e82ced9375077f1d720d0c1f19af192db4ec9
Oleksandr Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2026-02-16 10:10:33 +0200

    create project and add i18n

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+};
+
+export default async function LocaleLayout({ children, params }: Props) {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} h-screen antialiased bg-background`}>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
